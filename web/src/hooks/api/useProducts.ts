36bfdc8f58a796a865b8e8aae1ed2ca30f86d97c
Pablo <pablodosantos2007@gmail.com>
2026-05-19 02:46:11 -0300

    refactor(produto): extract product search from homepage to hook.

---
+++ web/src/hooks/api/useProducts.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+  type Category = {
+    id: number;
+    name: string;
+  };
+
+  type Product = {
+    id: number;
+    name: string;
+    category?: Category;
+    price: number;
+  };
+
+  export default function Home() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+
+      console.log("Buscando produtos...");
+
+      const res = await fetch("http://localhost:8081/products");
+
+      if (!res.ok) throw new Error("Erro ao buscar produtos");
+
+      const data: Product[] = await res.json();
+
+      console.log("Produtos recebidos:", data);
+
+      setProducts(data);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError("Não foi possível carregar os produtos");
+      } else {
+        setError("Erro desconhecido");
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  fetchProducts();
+}, []);
+
+ return {
+    products,
+    loading,
+    error,
+  };
+}
