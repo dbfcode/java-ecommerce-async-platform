cdd2262f17227235d01f11c55668437c019709d9
Pablo <pablodosantos2007@gmail.com>
2026-06-09 00:08:03 -0300

    chore(produto): remove checkout and email-confirm routes temporarily

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./app/HomePage/page"
-import CheckoutPage from "./app/CheckoutPage/page"
-import EmailConfirmPage from "./app/EmailConfirmPage/page"
- 
 export default function App() {
   return (  
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/email-confirm" element={<EmailConfirmPage />} />
       </Routes>
     </BrowserRouter>
   )
